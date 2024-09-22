185cb4f189a1831f64b285245c823bfb7fdeb825
Pedro <pedro.jimenezli@anahuac.mx>
2024-09-21 22:54:49 -0600

    Subida inicial del proyecto PlayCanvas

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2075045.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196649278, 196649279, 196649302, 196649282, 196649283, 196649284, 196649285, 196649286, 196649288, 196649289, 196649290, 196649291, 196649293, 196649294 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196649299/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196649300/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196649298/1/ammo.js', 'preload' : true},
+];
